6b68eb60f35dda575899a01c26ee4d03accd2113
Ayça Vural Andaç <ayca.vuralandac@tokeninc.com>
2022-02-05 14:30:55 +0300

    some fixes about response

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -1,18 +1,20 @@
 const serviceRecord = require('../services/record.service');
 const { Response } = require('../utils/response');
+const logger = require('../config/logger.config').log;
 
 module.exports = {
   async get(req, res) {
     const { body } = req;
     try {
       const result = await serviceRecord.findAll(body);
-      if (Object.prototype.hasOwnProperty.call(result, 'records')) {
-        const responseJson = { ...Response.SUCCESS };
+      responseJson = { ...Response.SUCCESS };
+      if (Array.isArray(result) && result.length > 0) {
         responseJson.records = result;
-        res.status(200).json(responseJson);
       }
+      res.status(200).json(responseJson);
     } catch (err) {
-      res.send(400);
+      logger.error(`Unexpected error during operation.`, err);
+      res.status(400).json(Response.ERROR_GENERAL);
     }
   },
 };
